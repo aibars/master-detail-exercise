aab2b981555b6646dc987f05e960bfe653547bcc
Agustín Ibars <agustin.ibars@gmail.com>
2019-12-02 17:32:59 -0300

    Fix styles and add more info to card

--- src/components/SelectedItem.js
+++ src/components/SelectedItem.js
@@ -13,18 +13,23 @@ export default function SelectedItem(props) {
                 (<div className="beast-info">
                     <br />
                     <span className="selected-name">Beast Name: {item.species.name[0].toUpperCase() + item.species.name.slice(1)}</span>
-                    <img width="150" height="150" src={item.sprites.front_default} alt={item.species.name} />
+                    <div className="img-container">
+                        <img width="150" height="150" src={item.sprites.front_default} alt={item.species.name} />
+                        <img width="150" height="150" src={item.sprites.back_default} alt={item.species.name} />
+                    </div>
                     <br />
                     Abilities:
                     <ul className="abilities-list">
                         {item.abilities.map((elem, i) => (
-                            <li key={i} className="ability">{elem.ability.name}</li>
+                            <li key={i} className="ability">{elem.ability.name} {elem.is_hidden ?
+                                <img className="hidden" height="25" width="25" src={require('../styles/hidden.png')} alt=""></img> : ''}
+                            </li>
                         ))}
                     </ul>
                     Stats:
                     <ul className="abilities-list">
                         {item.stats.map((elem, i) => (
-                            <li key={i} className="ability">{elem.stat.name}</li>
+                            <li key={i} className="ability">{elem.stat.name}: {elem.base_stat}</li>
                         ))}
                     </ul>
                 </div>)}
